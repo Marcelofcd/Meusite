eb1aec2819db3735e5ba2081ded952b82670df8f
Marcelofcd <65692996+Marcelofcd@users.noreply.github.com>
2021-01-11 10:21:18 -0300

    index iniciado

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var span = document.querySelector('.menu');
+
+span.addEventListener('click', function(){
+    var conteudo = document.querySelector('.conteudo p');
+
+    if (conteudo.classList.contains('mostrar')){
+        conteudo.classList.remove('mostrar');
+    } else {
+        conteudo.classList.add('mostrar');
+    }
+});
